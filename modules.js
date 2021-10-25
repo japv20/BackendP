cfee9ccb4ab734fe6de80a457d3040e3f65c953b
Jackeline <jackelineandrea58@hotmail.com>
2021-10-25 21:00:41 +0100

    modules

---
+++ modules.js
@@ -0,0 +1,8 @@
+export default class Plate {
+    constructor(mealName, mealDescription, mealPrice) {
+        this.mealName = mealName;
+        this.mealDescription = mealDescription;
+        this.mealPrice = mealPrice;
+    }
+}
+
