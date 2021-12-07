c606a0540718c116a0e38627696419fecf96b96a
Jackeline <jackelineandrea58@hotmail.com>
2021-12-07 21:00:36 +0000

    i

--- public/javascript/script.js
+++ public/javascript/script.js
@@ -62,39 +62,48 @@ document.addEventListener("DOMContentLoaded", (event) => {
             //console.log(supabase.auth.user().role) // console.log role of current user
             
             let listHolder = document.getElementById('menus');
+            data.forEach(item => {
+                listHolder.innerHTML += 
+                ` <h3> ${item.name} </h3>
+                <p> <i> ${item.category} </p> </i>
+                <p> ${item.description} </p>
+                <p> ${item.price} </p>
+                <img src=${item.picture}/>
+                `
+            }); // closing foreach loop
             // let anonHolder = document.getElementById('menus-for-anon')
             // two templates put them in a js folder 
-            data.forEach(item => {                
-                let templateForAnon = `
-                <section class="meal" id="${item.id}">
-                <h3> ${item.name} </h3>
-                <p> <i> ${item.category} </i> </p>
-                <p> ${item.description} </p>
-                <p> ${item.price} </p>
-                <img class="picture-container" src=${item.picture} alt="${item.name}"/> <br>
-                </section> `
+            // data.forEach(item => {                
+            //     let templateForAnon = `
+            //     <section class="meal" id="${item.id}">
+            //     <h3> ${item.name} </h3>
+            //     <p> <i> ${item.category} </i> </p>
+            //     <p> ${item.description} </p>
+            //     <p> ${item.price} </p>
+            //     <img class="picture-container" src=${item.picture} alt="${item.name}"/> <br>
+            //     </section> `
 
-                let templateForUser = `<section class="meal" id="${item.id}">
-                <h3> ${item.name} </h3>
-                <p> <i> ${item.category} </i> </p>
-                <p> ${item.description} </p>
-                <p> ${item.price} </p>
-                <img class="picture-container" src=${item.picture} alt="${item.name}"/> <br>
-                <button class="edit"> Edit </button>
-                <button class="delete"> Delete </button>
-                </section> `
+            //     let templateForUser = `<section class="meal" id="${item.id}">
+            //     <h3> ${item.name} </h3>
+            //     <p> <i> ${item.category} </i> </p>
+            //     <p> ${item.description} </p>
+            //     <p> ${item.price} </p>
+            //     <img class="picture-container" src=${item.picture} alt="${item.name}"/> <br>
+            //     <button class="edit"> Edit </button>
+            //     <button class="delete"> Delete </button>
+            //     </section> `
 
-                // anonHolder.innerHTML += templateForAnon
+            //     // anonHolder.innerHTML += templateForAnon
 
-                if (supabase.auth.user().role = 'authenticated') {
-                    listHolder.innerHTML += templateForUser;
-                    // anonHolder.style.display = "none";
-                } 
-                else {
-                    listHolder.innerHTML += templateForAnon;
-                }
+            //     if (supabase.auth.user().role = 'authenticated') {
+            //         listHolder.innerHTML += templateForUser;
+            //         // anonHolder.style.display = "none";
+            //     } 
+            //     else {
+            //         listHolder.innerHTML += templateForAnon;
+            //     }
 
-            }); //closing data for each loop
+            // }); //closing data for each loop
 
             const logoutButton = document.getElementById('logout')
             console.log(logoutButton)
@@ -123,8 +132,7 @@ document.addEventListener("DOMContentLoaded", (event) => {
                 formContainer.innerHTML = `
                 <span class="close">&times;</span>
                 <p> This is the id ${byMealID} </p>
-                <p> This is the tester ${editAction.parentNode.firstElementChild} </p>
-                
+                <p> This is the tester ${editAction.parentNode.firstElementChild} </p>                
                 `
             }
 
