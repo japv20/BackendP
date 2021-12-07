217f8deb6ad2ecd4460c88ed5611b27aa0b642af
Jackeline <jackelineandrea58@hotmail.com>
2021-12-07 22:06:37 +0000

    i

--- public/javascript/script.js
+++ public/javascript/script.js
@@ -158,6 +158,7 @@ document.addEventListener("DOMContentLoaded", (event) => {
             editButtons.forEach(editAction => {
                 editAction.addEventListener('click', (event) => {
                     event.preventDefault()
+                    console.log(editButtons.firstElementChild())
                     console.log(`you clicked me to delete ${editAction.parentNode.id} information`)
                     console.log(editAction.parentNode.outerText)
                     // console.log(editAction.parentNode.firstElementChild)
