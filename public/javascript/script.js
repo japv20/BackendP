6e0f1e6c74235a81db430957fb628a09f0c1ad84
Jackeline <jackelineandrea58@hotmail.com>
2021-12-07 20:52:40 +0000

    i

--- public/javascript/script.js
+++ public/javascript/script.js
@@ -62,42 +62,37 @@ document.addEventListener("DOMContentLoaded", (event) => {
             //console.log(supabase.auth.user().role) // console.log role of current user
             
             let listHolder = document.getElementById('menus');
-            let anonHolder = document.getElementById('menus-for-anon')
+            // let anonHolder = document.getElementById('menus-for-anon')
             // two templates put them in a js folder 
-            data.forEach(item => {
-                listHolder.innerHTML += `
-                <section class="meal" id="${item.id}>
+            data.forEach(item => {                
+                let templateForAnon = `
+                <section class="meal" id="${item.id}">
                 <h3> ${item.name} </h3>
-                </section>
-                `
-                // let templateForAnon = `
-                // <section class="meal" id="${item.id}">
-                // <h3> ${item.name} </h3>
-                // <p> <i> ${item.category} </i> </p>
-                // <p> ${item.description} </p>
-                // <p> ${item.price} </p>
-                // <img class="picture-container" src=${item.picture} alt="${item.name}"/> <br>
-                // </section> `
+                <p> <i> ${item.category} </i> </p>
+                <p> ${item.description} </p>
+                <p> ${item.price} </p>
+                <img class="picture-container" src=${item.picture} alt="${item.name}"/> <br>
+                </section> `
 
-                // let templateForUser = `<section class="meal" id="${item.id}">
-                // <h3> ${item.name} </h3>
-                // <p> <i> ${item.category} </i> </p>
-                // <p> ${item.description} </p>
-                // <p> ${item.price} </p>
-                // <img class="picture-container" src=${item.picture} alt="${item.name}"/> <br>
-                // <button class="edit"> Edit </button>
-                // <button class="delete"> Delete </button>
-                // </section> `
+                let templateForUser = `<section class="meal" id="${item.id}">
+                <h3> ${item.name} </h3>
+                <p> <i> ${item.category} </i> </p>
+                <p> ${item.description} </p>
+                <p> ${item.price} </p>
+                <img class="picture-container" src=${item.picture} alt="${item.name}"/> <br>
+                <button class="edit"> Edit </button>
+                <button class="delete"> Delete </button>
+                </section> `
 
                 // anonHolder.innerHTML += templateForAnon
 
-                // if (supabase.auth.user().role = 'authenticated') {
-                //     listHolder.innerHTML += templateForUser;
-                //     anonHolder.style.display = "none";
-                // } 
-                // else {
-                //     listHolder.innerHTML += templateForAnon;
-                // }
+                if (supabase.auth.user().role = 'authenticated') {
+                    listHolder.innerHTML += templateForUser;
+                    // anonHolder.style.display = "none";
+                } 
+                else {
+                    listHolder.innerHTML += templateForAnon;
+                }
 
             }); //closing data for each loop
 
