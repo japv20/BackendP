09e894a944486cf1b4c98a9931a862d43196f514
Jackeline <jackelineandrea58@hotmail.com>
2021-12-08 00:50:46 +0000

    i

--- public/javascript/script.js
+++ public/javascript/script.js
@@ -177,7 +177,7 @@ document.addEventListener("DOMContentLoaded", (event) => {
                     <p> This is the image ${editAction.parentNode.lastElementChild.innerHTML} </p>
                     `
 
-                    console.log(editAction.parentNode.childNodes[0])
+                    console.log(editAction.parentNode.childNodes[0].innerHTML)
 
                     window.onclick = function(event) {
                         if (event.target == modalUpdate) {
