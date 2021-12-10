71a7e97ae3e2d42c24a0b1720fbf49c8c02dcb24
Jackeline <jackelineandrea58@hotmail.com>
2021-12-10 14:03:32 +0000

    removing unused files

--- serverrrr.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-
-const app = express();
-
-app.get('188.166.172.132', (req, res) => {
-    res.send('Successful response.');
-});
-
-app.listen(3000, () => console.log('Example app is listening on port 3000.'));
